c3940acb5227f7fa11d5ea22e647220c91e3148f
YASWANTH RAM BALAJI DANGUDUBIYYAM <d.yaswanth9999@gmail.com>
2025-08-03 11:52:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -23,13 +23,17 @@ Avatar.displayName = AvatarPrimitive.Root.displayName
 const AvatarImage = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Image>,
   React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
->(({ className, ...props }, ref) => (
-  <AvatarPrimitive.Image
-    ref={ref}
-    className={cn("aspect-square h-full w-full", className)}
-    {...props}
-  />
-))
+>(({ className, src, ...props }, ref) => {
+    if (!src) return null;
+    return (
+        <AvatarPrimitive.Image
+            ref={ref}
+            src={src}
+            className={cn("aspect-square h-full w-full", className)}
+            {...props}
+        />
+    )
+})
 AvatarImage.displayName = AvatarPrimitive.Image.displayName
 
 const AvatarFallback = React.forwardRef<
